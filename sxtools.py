ab242082b931476cc984bb0fe0add6049022bdef
Jani <jani@secretexit.com>
2019-10-04 13:05:15 +0300

    * Updated todo

--- sxtools.py
+++ sxtools.py
@@ -5535,6 +5535,9 @@ if __name__ == '__main__':
 
 
 # TODO:
+# - Add alpha support to debug mode
+# - Crease fails with face selection (no, fails with extrusion performed without going obj/edit)
+# - If vehicle and _wheel in name, only local AO
 # - Per-object prop for overlay strength
 # - Create and re-index UV0 if not present in processing
 # - Auto-place pivots during processing?
